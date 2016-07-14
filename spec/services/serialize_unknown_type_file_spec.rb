012e039a04f8595675eaf89e6db76489aff2ed31
Justin Gondron <jgondron@nd.edu>
2016-07-14 16:30:33 -0400

    DEC-1143: Hound

--- spec/services/serialize_unknown_type_file_spec.rb
+++ spec/services/serialize_unknown_type_file_spec.rb
@@ -1,15 +1,22 @@
 require "rails_helper"
 
 describe SerializeUnknownTypeFile do
-  let(:params) {{ media_type: "media type" }}
-  let(:attributes) {{ "id" => 1, "uuid" => "87ea8e9c-5932-478e-95d8-aeb04888a89a", "media_type" => "media type", "file_path" => "file path" }}
+  let(:params) { {media_type: "media type"} }
+  let(:attributes) do
+    {
+      "id" => 1,
+      "uuid" => "87ea8e9c-5932-478e-95d8-aeb04888a89a",
+      "media_type" => "media type",
+      "file_path" => "file path"
+    }
+  end
   let(:media) { MediaFile.new(attributes) }
 
   describe "to_hash" do
     let(:subject) { SerializeUnknownTypeFile.to_hash(object: media) }
 
     it "excludes id" do
-      expect(subject).not_to include(:"id")
+      expect(subject).not_to include(:id)
     end
 
     it "includes all other attributes" do
@@ -21,11 +28,11 @@ describe SerializeUnknownTypeFile do
     let(:subject) { JSON.parse(SerializeUnknownTypeFile.to_json(object: media), symbolize_names: true) }
 
     it "excludes id" do
-      expect(subject).not_to include(:"id")
+      expect(subject).not_to include(:id)
     end
 
     it "includes all other attributes" do
-      expect(subject).to include(:"file_path", :"media_type")
+      expect(subject).to include(:file_path, :media_type)
     end
   end
 end
