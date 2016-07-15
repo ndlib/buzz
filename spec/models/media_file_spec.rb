3d261e61609e1eaa849cb5d904d0e5c82b08fa2c
Justin Gondron <jgondron@nd.edu>
2016-07-15 09:39:39 -0400

    DEC-1143: Adding specs for MediaFile validation

---
+++ spec/models/media_file_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe MediaFile do
+  it "requires the file_path field" do
+    expect(subject).to have(1).errors_on(:file_path)
+  end
+
+  it "requires the uuid field" do
+    expect(subject).to have(1).errors_on(:uuid)
+  end
+
+  describe "media_type validator" do
+    it "requires a value" do
+      expect(subject).to have(1).errors_on(:media_type)
+    end
+
+    it "accepts video" do
+      subject.media_type = "video"
+      expect(subject).to have(0).errors_on(:media_type)
+    end
+
+    it "accepts audio" do
+      subject.media_type = "audio"
+      expect(subject).to have(0).errors_on(:media_type)
+    end
+
+    it "does not accept empty string" do
+      subject.media_type = ""
+      expect(subject).to have(1).errors_on(:media_type)
+    end
+
+    it "does not accept unknown value" do
+      subject.media_type = "something"
+      expect(subject).to have(1).errors_on(:media_type)
+    end
+  end
+end
