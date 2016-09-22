551026a0c78831648bf850a10bfe903d49b23aae
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-09-22 14:08:36 -0400

    try to set ssl

--- app/services/create_buzz_url.rb
+++ app/services/create_buzz_url.rb
@@ -2,7 +2,7 @@ class CreateBuzzUrl
   def self.call(uri = nil)
     host = Rails.application.routes.default_url_options[:host]
     port = Rails.application.routes.default_url_options[:port]
-    protocol = Rails.application.routes.default_url_options[:protocol]
+    protocol = "https" # Rails.application.routes.default_url_options[:protocol]
 
     port_suffix = ":#{port}" if port.present?
     uri_suffix = "/#{uri}" if uri.present?
