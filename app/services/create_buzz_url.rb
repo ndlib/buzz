692949bbc983437c975c78c72ef31e2b39ee223a
Jonathan Hartzler <Jonathan.Hartzler.3@nd.edu>
2016-09-22 14:48:07 -0400

    fix the protocol

--- app/services/create_buzz_url.rb
+++ app/services/create_buzz_url.rb
@@ -2,11 +2,15 @@ class CreateBuzzUrl
   def self.call(uri = nil)
     host = Rails.application.routes.default_url_options[:host]
     port = Rails.application.routes.default_url_options[:port]
-    protocol = "https" # Rails.application.routes.default_url_options[:protocol]
 
     port_suffix = ":#{port}" if port.present?
     uri_suffix = "/#{uri}" if uri.present?
 
     "#{protocol}://#{host}#{port_suffix}#{uri_suffix}"
+  end
+
+  def self.protocol
+    return "http" if ["development", "test"].include?(Rails.env)
+    "https"
   end
 end
--- spec/services/create_buzz_url_spec.rb
+++ spec/services/create_buzz_url_spec.rb
@@ -19,4 +19,24 @@ describe CreateBuzzUrl do
   it "appends the given uri" do
     expect(CreateBuzzUrl.call("my/uri")).to eq("http://test.com:1000/my/uri")
   end
+
+  it "returns http for development env" do
+    allow(Rails).to receive(:env).and_return("development")
+    expect(CreateBuzzUrl.protocol).to eq("http")
+  end
+
+  it "returns http for test env" do
+    allow(Rails).to receive(:env).and_return("test")
+    expect(CreateBuzzUrl.protocol).to eq("http")
+  end
+
+  it "returns https for pre_production env" do
+    allow(Rails).to receive(:env).and_return("pre_production")
+    expect(CreateBuzzUrl.protocol).to eq("https")
+  end
+
+  it "returns https for production env" do
+    allow(Rails).to receive(:env).and_return("production")
+    expect(CreateBuzzUrl.protocol).to eq("https")
+  end
 end
