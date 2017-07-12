f1d9c57edd658a4ecfe71f2a1b76ad5cd94a15ec
Dan Wolfe <dwolfe2@nd.edu>
2017-07-12 14:42:30 -0400

    fix tests

--- spec/services/create_content_url_spec.rb
+++ spec/services/create_content_url_spec.rb
@@ -20,12 +20,12 @@ describe CreateContentUrl do
     let(:object) { instance_double(MediaFile, media_type: "video", file_path: "file-path") }
 
     it "renders the correct http url" do
-      url = "http://wowza-test.com:1000/wowie/instance/mp4:source-prefix/source/file-path/playlist.m3u8"
+      url = "https://wowza-test.com:1000/wowie/instance/mp4:source-prefix/source/file-path/playlist.m3u8"
       expect(CreateContentUrl.http(object: object)).to eq(url)
     end
 
     it "renders the correct rtmp url" do
-      url = "rtmp://wowza-test.com:1000/wowie/instance/mp4:source-prefix/source/file-path"
+      url = "rtmps://wowza-test.com:1000/wowie/instance/mp4:source-prefix/source/file-path"
       expect(CreateContentUrl.rtmp(object: object)).to eq(url)
     end
   end
@@ -34,12 +34,12 @@ describe CreateContentUrl do
     let(:object) { instance_double(MediaFile, media_type: "audio", file_path: "file-path") }
 
     it "renders the correct http url" do
-      url = "http://wowza-test.com:1000/wowie/instance/mp3:source-prefix/source/file-path/playlist.m3u8"
+      url = "https://wowza-test.com:1000/wowie/instance/mp3:source-prefix/source/file-path/playlist.m3u8"
       expect(CreateContentUrl.http(object: object)).to eq(url)
     end
 
     it "renders the correct rtmp url" do
-      url = "rtmp://wowza-test.com:1000/wowie/instance/mp3:source-prefix/source/file-path"
+      url = "rtmps://wowza-test.com:1000/wowie/instance/mp3:source-prefix/source/file-path"
       expect(CreateContentUrl.rtmp(object: object)).to eq(url)
     end
   end
@@ -48,12 +48,12 @@ describe CreateContentUrl do
     let(:object) { instance_double(MediaFile, media_type: "audio", file_path: "file-path.m4a") }
 
     it "renders the correct http url" do
-      url = "http://wowza-test.com:1000/wowie/instance/mp4:source-prefix/source/file-path.m4a/playlist.m3u8"
+      url = "https://wowza-test.com:1000/wowie/instance/mp4:source-prefix/source/file-path.m4a/playlist.m3u8"
       expect(CreateContentUrl.http(object: object)).to eq(url)
     end
 
     it "renders the correct rtmp url" do
-      url = "rtmp://wowza-test.com:1000/wowie/instance/mp4:source-prefix/source/file-path.m4a"
+      url = "rtmps://wowza-test.com:1000/wowie/instance/mp4:source-prefix/source/file-path.m4a"
       expect(CreateContentUrl.rtmp(object: object)).to eq(url)
     end
   end
