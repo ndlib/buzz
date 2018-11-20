0aa567634b632731a207fcfc59c1963f8d141c2d
Justin Gondron <jgondron@nd.edu>
2018-11-20 16:48:28 -0500

    - Reconfigured the puma.rb to default to only binding on tcp:3000 and changed the Dockerfile to explicitly bind to the unix socket, since we only care about it running this way within the context of the container - Removed the default port stuff from the Dockerfile since we are only binding to socket now - Removing some left over from my previous commit

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,3 +1,3 @@
 echo Mapping SSM path "$ENV_SSM_PATH" to my environment.
 service nginx start
-exec bundle exec puma
+exec bundle exec puma -b "unix://${APP_HOME}/shared/sockets/puma.sock"
--- docker/ssl_cmd.sh
+++
@@ -1,2 +0,0 @@
-echo Mapping SSM path "$ENV_SSM_PATH" to my environment.
-exec chamber exec "$ENV_SSM_PATH" -- bundle exec puma -b "ssl://0.0.0.0:$PORT?key=self_signed.key&cert=self_signed.cert"
