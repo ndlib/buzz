ad3e01e1c309db52cc2e805d2945e6d7bcb2c458
Justin Gondron <jgondron@nd.edu>
2018-11-27 11:18:23 -0500

    - Separated the docker scripts into an entry and command. This should allow us to start the nginx+puma services by default, but override it in order to do things like bundle exec db:migrate when needed. The entry script handles the chamber env, so in either case all of that will be available to the bundle commands when overriding. - Removed docker/dev_env, we aren't using this atm here - Cleaned up a copy/paste comment in the dockerfile

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,3 +1,2 @@
 echo Mapping SSM path "$ENV_SSM_PATH" to my environment.
-service nginx start
-exec bundle exec puma -b "unix://${APP_HOME}/shared/sockets/puma.sock"
+exec chamber exec $ENV_SSM_PATH -- "$@"
