0afa0fc424eb249bf5b3629e43e1ee0be760cb5b
Justin Gondron <jgondron@nd.edu>
2021-04-15 10:11:35 -0400

    Add rails migration entry
    This adds an entry point for doing rails migrations. This is similar to
    the entry point for the rails service, except it does not need to start
    rails or nginx, so made sense to separate these out.
    
    Additional changes:
    - Changed the start services script to be named rails_entry.sh and use
    a wait for db line to match how we're doing other services
    - Simplified required newman params. Since we require https, seemed
    overkill to require this as a param

--- docker/rails_entry.sh
+++ docker/rails_entry.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+if ! docker/wait-for-it.sh -t 120 ${DB_HOSTNAME}:5432; then exit 1; fi
+
 # Runs nginx + puma services
 service nginx start
 bundle exec puma -b "unix://${APP_HOME}/shared/sockets/puma.sock"
